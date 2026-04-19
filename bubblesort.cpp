ae8a32ca628664d7dab3f6f5350cbd6a5443388b
Siddharth Gupta <sid20060329@gmail.com>
2026-04-19 12:00:44 +0530

    bubblesort program

---
+++ bubblesort.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int n){
+    for (int i=0;i<n-1;++i){
+        bool isSwap = false;
+        for(int j=0;j<n-i-1;++j){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+                isSwap = true;
+            }
+        }
+        if(!isSwap){
+            return;
+        }
+    }
+}
+
+void printArray(int arr[],int n){
+    for(int i=0;i<n;i++){
+        cout << arr[i]<<", ";
+    }
+}
+
+int main(){
+    int arr[] = {1,2,3,566,43,52,-5};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    bubbleSort(arr, n);
+    printArray(arr, n);
+    return 0;
+}
